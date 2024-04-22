2b0cf8b72f4569a33441f5928abd034bc9910d64
Keith Hall <hkeith62@gmail.com>
2024-04-22 10:37:02 -0500

    Initial push

---
+++ constants/images.js
@@ -0,0 +1,116 @@
+/**
+ * Title: Imagaes.js
+ * Author: https://www.youtube.com/watch?v=3HNyXCPDQ7Q
+ * Date: 03/10/2024
+ * Modified by: Keith Hall
+ * Description: Js file for importing and exporting all images. 
+ */
+import headphone_24b from './assets/headphone_24b.webp';
+import watch_3 from './assets/watch_3.webp';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import javascript01 from '../assets/javascript01.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+import about05 from '../assets/about05.png';
+import about06 from '../assets/about06.png';
+import about08 from '../assets/about08.png';
+import about09 from '../assets/about09.png';
+import about10 from '../assets/about10.png';
+import about14 from '../assets/about14.png';
+import work01 from '../assets/work01.png';
+import work02 from '../assets/work02.png';
+import work03 from '../assets/work03.png';
+import work04 from '../assets/work04.png';
+import work05 from '../assets/work05.png';
+import work06 from '../assets/work06.png';
+import work10 from '../assets/work10.png';
+import work11 from '../assets/work11.png';
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+import nb from '../assets/nb.png';
+import skype from '../assets/skype.png';
+import spotify from '../assets/spotify.png';
+import favicon from '../assets/favicon.png';
+import networks from '../assets/networks.png';
+import compliance from '../assets/compliance.png';
+
+
+//Export all images.
+export default {
+  headphone_24b,
+  watch_3,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  javascript01,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  about05,
+  about06,
+  about08,
+  about09,
+  about10,
+  about14,
+  work01,
+  work02,
+  work03,
+  work04,
+  work05,
+  work06,
+  work10,
+  work11,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+  favicon,
+  networks,
+  compliance
+};
+ 
